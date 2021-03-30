ca43f1a81e04d09f70d339ee9486902f921bcd1d
Karen Valadez <valadez.karen04@gmail.com>
2021-03-30 14:44:23 -0500

    refactored exercise.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -8,24 +8,36 @@
 // not done ^^
 
 //}
+//var addNumber = parseInt(prompt("Choose an ODD number between 1-50: "));
 
-// another example
-var skipHere;
-while (skipHere !== "Never going too equal to this,") {
-    skipHere = parseFloat(prompt("insert an ODD number between 1-50: "));
-    if (skipHere % 2 === 1) {
-        console.log("Breaak!");
+
+var addNumber;
+addNumber = parseInt(prompt("Choose an ODD number between 1-50: "));
+while (true) {
+    if (addNumber % 2 === 1 || (addNumber > 0 && addNumber < 50)) {
         break;
     }
 }
+for (var i = 1; i < 50; i+=) {
 
-console.log("Number to skip is " + skipHere);
-console.log("");
-
-for( var i = 1; i < 50; i+=2) {
-    if (i === skipHere) {
-        console.log("Yikes! Skipping this number: " + i);
-    }
 }
+// another example
+// var skipHere;
+// while (skipHere !== "Never going too equal to this,") {
+//     skipHere = parseFloat(prompt("insert an ODD number between 1-50: "));
+//     if (skipHere % 2 === 1) {
+//         console.log("Breaak!");
+//         break;
+//     }
+// }
+//
+// console.log("Number to skip is " + skipHere);
+// console.log("");
+//
+// for( var i = 1; i < 50; i+=2) {
+//     if (i === skipHere) {
+//         console.log("Yikes! Skipping this number: " + i);
+//     }
+// }
 
 //while (isNaN(skippingNumber) || skippingNumber % 2 !== 1);
