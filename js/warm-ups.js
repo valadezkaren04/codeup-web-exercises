4a11acc99896c17118713a549d97b8f4745e5df6
Karen Valadez <valadez.karen04@gmail.com>
2021-05-24 09:32:53 -0500

    added the word problem to the warm up

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -1,6 +1,16 @@
 "use strict";
 
 //05.24.21
+/**
+ *
+ * Often in programming interviews, you may be asked to solve a problem called “FizzBuzz.” FizzBuzz is a test of your ability to
+ * use conditional logic with a loop that counts up by one with every iteration. The challenge asks that you log the word “Fizz” if
+ * a number is evenly divisible by 3, “Buzz” if a number is evenly divisible by 5, “FizzBuzz” if it is divisible by both 3 and 5,
+ * and finally the number if it is not divisible by 3 or 5.
+ *
+ * This morning, I’d like you to write FizzBuzz in JavaScript using a for loop that goes until the number 30.
+ *
+ */
 for (var i = 1; i <= 30; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
