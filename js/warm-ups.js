2db403714ef4b7092daa3de9199ff77ded990ed5
Karen Valadez <valadez.karen04@gmail.com>
2021-05-24 09:28:14 -0500

    warm up completed

---
+++ js/warm-ups.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//05.24.21
+for (var i = 1; i <= 30; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
